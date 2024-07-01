0c1cefe9cac85a354496133b2d82e32b343a545a
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-07-01 19:19:07 +0900

    add output test

--- 01.fizzbuzz/test/fizzbuzz.test.js
+++ 01.fizzbuzz/test/fizzbuzz.test.js
@@ -1,9 +1,9 @@
 import { test } from "node:test";
 import assert from "node:assert/strict";
 
-import { prepare } from "../fizzbuzz.js";
+import { prepare_fizzbuzz_results, output_fizzbuzz } from "../fizzbuzz.js";
 
-test("fizzbuzz", () => {
+test("prepare_fizzbuzz_results", () => {
   const expected = [
     "1",
     "2",
@@ -26,5 +26,43 @@ test("fizzbuzz", () => {
     "19",
     "Buzz",
   ];
-  assert.deepEqual(prepare(), expected);
+  assert.deepEqual(prepare_fizzbuzz_results(20), expected);
+});
+
+test("output_fizzbuzz", () => {
+  const expected = [
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz",
+    "16",
+    "17",
+    "Fizz",
+    "19",
+    "Buzz",
+  ].join("\n");
+
+  const original_console_log = console.log;
+  let output = "";
+
+  console.log = (message) => {
+    output += message + "\n";
+  };
+
+  output_fizzbuzz(20);
+  output = output.trim();
+  console.log = original_console_log;
+
+  assert.strictEqual(output, expected);
 });
