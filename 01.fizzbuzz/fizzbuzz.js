0aaf841f642046278e1beac9194412159a8e9cdb
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-07-01 19:18:46 +0900

    improve fizzbuzz

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -3,29 +3,30 @@
 import { fileURLToPath } from "url";
 import process from "process";
 
-export function prepare() {
-  const results = [];
-  for (let i = 1; i <= 20; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      results.push("FizzBuzz");
-    } else if (i % 3 === 0) {
-      results.push("Fizz");
-    } else if (i % 5 === 0) {
-      results.push("Buzz");
+export function prepare_fizzbuzz_results(max_num) {
+  const results = [...Array(max_num).keys()].map((i) => {
+    const num = i + 1;
+    if (num % 3 === 0 && num % 5 === 0) {
+      return "FizzBuzz";
+    } else if (num % 3 === 0) {
+      return "Fizz";
+    } else if (num % 5 === 0) {
+      return "Buzz";
     } else {
-      results.push(i.toString());
+      return num.toString();
     }
-  }
+  });
+
   return results;
 }
 
-function exec() {
-  const results = prepare();
+export function output_fizzbuzz(max_num) {
+  const results = prepare_fizzbuzz_results(max_num);
   results.forEach((result) => {
     console.log(result);
   });
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  exec();
+  output_fizzbuzz(20);
 }
