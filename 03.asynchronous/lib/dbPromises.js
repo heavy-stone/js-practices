b331f00ac5d8dd20e6e4c83aaf6a2c3bb0df0f9c
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-08-26 16:45:23 +0900

    add callback operation to arguments in dbPromises

--- 03.asynchronous/lib/dbPromises.js
+++ 03.asynchronous/lib/dbPromises.js
@@ -1,6 +1,13 @@
 export function dbRunPromise(db, sql, ...params) {
+  let callback;
+  if (params.length > 0 && typeof params[params.length - 1] === "function") {
+    callback = params.pop();
+  }
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (err) {
+      if (callback) {
+        callback(err);
+      }
       if (err) {
         reject(err);
       } else {
@@ -11,8 +18,15 @@ export function dbRunPromise(db, sql, ...params) {
 }
 
 export function dbGetPromise(db, sql, ...params) {
+  let callback;
+  if (params.length > 0 && typeof params[params.length - 1] === "function") {
+    callback = params.pop();
+  }
   return new Promise((resolve, reject) => {
     db.get(sql, params, (err, row) => {
+      if (callback) {
+        callback(err, row);
+      }
       if (err) {
         reject(err);
       } else {
@@ -22,9 +36,12 @@ export function dbGetPromise(db, sql, ...params) {
   });
 }
 
-export function dbClosePromise(db) {
+export function dbClosePromise(db, callback = null) {
   return new Promise((resolve, reject) => {
     db.close((err) => {
+      if (callback && typeof callback === "function") {
+        callback(err);
+      }
       if (err) {
         reject(err);
       } else {
